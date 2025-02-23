4da7485574c5999b361067fcc123f281672ad715
Vuyko Sheshych <vuykosheshych@gmail.com>
2025-02-23 22:56:55 +0200

    chore(db): update migrations

---
+++ Data/Migrations/20250223204845_EditMovesAgain.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GamePlayService.Migrations
+{
+    /// <inheritdoc />
+    public partial class EditMovesAgain : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
